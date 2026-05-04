2d0fa32327c7ceda3439d8d9d9527c7c815b9590
JunHyeop3631 <sstechangle@gmail.com>
2026-05-05 03:52:04 +0900

    test

--- KraftonEngine/LuaScripts/Game/RowGenerator.lua
+++ KraftonEngine/LuaScripts/Game/RowGenerator.lua
@@ -161,14 +161,14 @@ function RowGenerator.GenerateRow(rowIndex)
         local interval = 0
 
         -- 2. 뽑힌 차량의 종류에 따라 속도와 스폰 주기를 다르게 세팅
-        if selectedVehicle.type == PREFABS.RACINGCAR or selectedVehicle.type == PREFABS.POLICECAR then
+        if selectedVehicle.type == PREFABS.RACINGCAR or selectedVehicle.type == PREFABS.FIRECAR then
             -- 스포츠카, 경찰차: 매우 빠른 속도, 짧은 간격
-            speed = 10.0 + (rowIndex * 0.05)
+            speed = 20.0 + (rowIndex * 0.05)
             interval = math.max(4.0, 10.0 - (rowIndex * 0.015))
 
-        elseif selectedVehicle.type == PREFABS.MINIBUS or selectedVehicle.type == PREFABS.FIRECAR then
+        elseif selectedVehicle.type == PREFABS.MINIBUS or selectedVehicle.type == PREFABS.POLICECAR then
             -- 버스, 소방차: 느린 속도, 넓은 간격
-            speed = 1.25 + (rowIndex * 0.008)
+            speed = 1.0 + (rowIndex * 0.004)
             interval = math.max(2.0, 10.0 - (rowIndex * 0.03))
 
         else
