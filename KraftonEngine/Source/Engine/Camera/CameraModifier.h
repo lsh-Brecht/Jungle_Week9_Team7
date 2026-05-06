a50c8cbd6d8d8d78b559f33aa28b9025894f253a
LEE SangHoon <hoon15x@gmail.com>
2026-05-06 14:31:07 +0900

    feat: add CameraModifier
    Add basic skeleton structure

---
+++ KraftonEngine/Source/Engine/Camera/CameraModifier.h
@@ -0,0 +1,53 @@
+﻿#pragma once
+
+#include "Object/Object.h"
+#include "Camera/CameraTypes.h"
+
+class FPlayerCameraManager;
+
+class UCameraModifier : public UObject
+{
+public:
+	DECLARE_CLASS(UCameraModifier, UObject)
+	UCameraModifier() = default;
+	~UCameraModifier() = default;
+
+	void Initialize(FPlayerCameraManager* Owner);
+
+	//per frame, update alpha, call modifyCamera
+	//비활성화를 반영해주며 alpha 값에 따라 ModifyCamera 를 호출함
+	bool UpdateCameraModifier(float DeltaTime, FCameraView& InOutView);
+	bool ModifyCamera(float DeltaTime, FCameraView& InOutView);
+
+	//D
+	void EnableModifier();
+	void DisableModifier(bool bRemoveAfterFadeOut);
+
+	//제거 대기 상태로 전환.
+	void MarkPendingRemove();
+
+	// --- Get , Set ---
+	void SetAlpha(float InAlpha) { Alpha = InAlpha; }
+	void SetAlphaInTime(float InTime) { AlphaInTime = InTime; }
+	void SetAlphaOutTime(float InTime) { AlphaOutTime = InTime; }
+	void SetDisabled(bool bInDisabled) { bDisabled = bInDisabled; }
+	void SetPriority(uint8 InPriority) { Priority = InPriority; }
+
+	float GetAlpha() const { return Alpha; }
+	float GetAlphaInTime() const { return AlphaInTime; }
+	float GetAlphaOutTime() const{return AlphaOutTime;}
+	bool  IsDisabled() const { return bDisabled != 0; }
+	uint8 GetPriority() const { return Priority; }
+
+	bool IsPendingRemove() const { return bPendingRemove != 0; }
+	bool IsShouldDestroy() const { return IsPendingRemove() && Alpha <= 0.0f; }
+
+public:
+	FPlayerCameraManager* CameraOwner = nullptr;
+	float Alpha = 0.0f;
+	float AlphaInTime = 0.0f;	//alpha 값이 올라가는 시간
+	float AlphaOutTime = 0.0f;	//alpha 값이 내려가는 시간
+	uint32 bDisabled = 0;
+	uint32 bPendingRemove = 0;	//true라면 효과가 완전히 꺼졌을 때 순회 리스트에서 제거
+	uint8 Priority = 0.;		//적용 순서
+};
